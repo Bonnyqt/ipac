8c68da27a913260c12e033d0b7bba77e43364355
Bonnyqt <marquezjonbon@gmail.com>
2025-07-18 02:17:03 +0800

    db

--- ipac/urls.py
+++ ipac/urls.py
@@ -10,5 +10,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        re_path(r'^static/(?P<path>.*)$', serve, {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    ]
